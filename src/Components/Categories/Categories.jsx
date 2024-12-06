8c613adc33861d6a245416aca5433fbb49b1024d
Enamul-Haque-Shojib <enamulhaqueshojib@gmail.com>
2024-12-06 08:31:41 +0600

    equipment table added

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -12,6 +12,12 @@ return (
     <div className="bg-gray-100 p-5 rounded-lg shadow-lg lg:w-[20%] w-full">
       <h2 className="text-xl font-semibold mb-4 text-gray-800">Categories</h2>
       <div className="flex flex-col gap-4">
+          <div
+              onClick={() => handleCategories('All')}
+              className="p-3 bg-white border rounded-lg shadow-sm cursor-pointer hover:shadow-md transition"
+            >
+              <h3 className="text-gray-800 font-medium">All</h3>
+            </div>
         {categories.map((category) => (
           <Category
             key={category._id}
